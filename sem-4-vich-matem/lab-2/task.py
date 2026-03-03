f3e0ba875018fb166667cf4c5b3fc1475c9d0941
Badulin Ilya <227145436+anodev996@users.noreply.github.com>
2026-03-03 21:54:34 +0700

    feat(sem-4-vich-matem): add lab 2

---
+++ sem-4-vich-matem/lab-2/task.py
@@ -0,0 +1,97 @@
+# vichislitelnaya matematika
+# variant 5
+
+'''
+import math
+
+a = -1
+b = 1
+
+
+def f(x):
+    return 4 * x + math.e ** x
+
+def f_prime(x):
+    return 4 + math.e ** x
+
+def f_double_prime(x):
+    return math.e ** x
+
+def initial_guess(a, b):
+    if f(a) * f_double_prime(a) > 0:
+        return a
+    else:
+        return b
+
+x0 = initial_guess(a, b)
+x1 = initial_guess(b, a)
+
+def delta_xn(x0, x1):
+    return ( f(x1) - f(x0) ) / ( x1 - x0 )
+
+def iteration_formula(x0, x1):
+    return x1 - ( f(x1) / delta_xn(x0, x1) ) 
+
+eps = 0.00001
+iteraton = 0
+iteration_max = 100
+root = 0
+while abs( f( iteration_formula(x0, x1) ) ) < eps:
+    root = iteration_formula(x0, x1)
+    iteration =+ 1
+
+print("root: {:.2f}\neps: {}\niterations: {}\n".format(root, eps, iteration))
+'''
+
+import math
+
+a = -1
+b = 1
+
+def f(x):
+    return 4 * x + math.e ** x
+
+def f_prime(x):
+    return 4 + math.e ** x
+
+def f_double_prime(x):
+    return math.e ** x
+
+def initial_guess(a, b):
+    if f(a) * f_double_prime(a) > 0:
+        return a
+    else:
+        return b
+
+x0 = initial_guess(a, b)
+x1 = initial_guess(b, a)
+
+def delta_xn(x0, x1):
+    if abs(x1 - x0) < 1e-15:
+        return f_prime(x1)
+    return (f(x1) - f(x0)) / (x1 - x0)
+
+def iteration_formula(x0, x1):
+    delta = delta_xn(x0, x1)
+    if abs(delta) < 1e-15:
+        return x1 - f(x1) / f_prime(x1)
+    return x1 - (f(x1) / delta)
+
+eps = 0.00000000001
+iteration = 0
+max_iterations = 10000
+for iteration in range(max_iterations):
+    x2 = iteration_formula(x0, x1)
+    
+    if abs(x2 - x1) < eps:
+        break
+        
+    if abs(f(x2)) < eps:
+        break
+    
+    x0, x1 = x1, x2
+    iteration += 1
+
+root = x2
+
+print("root: {:.10f}\neps: {:.10f}\niterations: {}\n".format(root, eps, iteration))
