3c75c425a5bf275fdb7372337cd56eb8c3e6f4e4
Badulin Ilya <227145436+anodev996@users.noreply.github.com>
2026-03-05 22:48:25 +0700

    feat(sem-4-vich-matem): lab 2 update

--- sem-4-vich-matem/lab-2/main.py
+++ sem-4-vich-matem/lab-2/main.py
@@ -2,20 +2,34 @@
 # variant 5
 import task_1 as t1
 
-A = -1
-B = 1
-EPS = 0.00001
-ITERATION_MAX = 100
+A: float = -1.0
+B: float = 1.0
+EPS: float = 0.00001
+ITERATION_MAX: int = 10
 
-MSG_1 = "Variant 5: Secant method"
-MSG_2 = "Variant 5: Golden-section search"
-DELIM = "-" * len(MSG_2)
+MSG_1: str = "Variant: #5 (Secant method)"
+MSG_2: str = "Variant: #5 (Golden-section search)"
+MSG_3: str = f"Note: Method didn't find root in {ITERATION_MAX} iterations"
+DELIM: str = "-" * len(MSG_3)
 
 
 print(f"{DELIM}\n{MSG_1}")
-result = []
+
+success, root, iteration = t1.secant_method(A, B, EPS, ITERATION_MAX)
+
+if success:
+    print(f"{DELIM}\nroot: {root:.10f}\n" +
+          f"epsilon: {EPS:.10f}\niterations done: {iteration}\n{DELIM}")
+else:
+    print(f"{DELIM}\n{MSG_3}\n{DELIM}\nlast approximation: {root:.10f}\n" +
+          f"epsilon: {EPS:.10f}\niterations done: {iteration}\n{DELIM}")
+
+
+'''
+print(f"{DELIM}\n{MSG_2}")
+
 result = t1.secant_method(A, B, EPS, ITERATION_MAX)
 
-
-print(f"{DELIM}\nroot: {result[0]:.10f}\neps: {EPS:.10f}\n" +
-      f"iterations: {result[1]}\n{DELIM}")
+print(f"{DELIM}\nroot: {root:.10f}\neps: {EPS:.10f}\n" +
+      f"iterations: {iteration}\n{DELIM}")
+'''
