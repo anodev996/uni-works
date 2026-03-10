32b38cee6ea1a35a13bc5ecf1c5f0ae2abf65614
Badulin Ilya <227145436+anodev996@users.noreply.github.com>
2026-03-11 04:53:54 +0700

    style(sem 4 vrnp): change code style according to Black formatter

--- sem-4-vrnp/practice-1/task-4.py
+++ sem-4-vrnp/practice-1/task-4.py
@@ -11,12 +11,14 @@ def print_list_matrix(matrix_num, list_n, list_matrix):
 
         for line in range(list_n[matrix_index]):
 
-            list_f_str = [f"{elem:6.2f}" for elem in list_matrix[matrix_index][line]]
+            list_f_str = [
+                f"{elem:6.2f}" for elem in list_matrix[matrix_index][line]
+            ]
             print(" ".join(list_f_str))
-            #print(list_f_str)
+            # print(list_f_str)
 
 
-'''
+"""
 list_n = []
 list_m = []
 list_matrix = []
@@ -27,27 +29,13 @@ for i in range(MATRIX_NUM):
     list_n.append(n)
     list_m.append(m)
     list_matrix.append(matrix)
-'''
+"""
 list_n = [2, 3, 4]
 list_m = [2, 2, 3]
 list_matrix = [
-    [
-        [1, 2],
-        [3, 4]
-    ],
-
-    [
-        [0, 2],
-        [3, -5],
-        [-3, 0]
-    ],
-    
-    [
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, 2, 3.162]
-    ]
+    [[1, 2], [3, 4]],
+    [[0, 2], [3, -5], [-3, 0]],
+    [[1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3.162]],
 ]
 
 
@@ -56,15 +44,17 @@ print_list_matrix(MATRIX_NUM, list_n, list_matrix)
 
 
 for matrix_index in range(MATRIX_NUM):
-    
+
     n = list_n[matrix_index]
     m = list_m[matrix_index]
     square_of_last_elem = list_matrix[matrix_index][n - 1][m - 1] ** 2
     if square_of_last_elem == 0:
-        print(f"\nmatrix #{matrix_index + 1}: Last number of the array can't be 0!\n" +
-              "           Skipping that matrix...\n")
+        print(
+            f"\nmatrix #{matrix_index + 1}: Last number of the array can't be 0!\n"
+            + "           Skipping that matrix...\n"
+        )
         continue
-    
+
     for line in range(n):
         for elem in range(m):
             list_matrix[matrix_index][line][elem] /= square_of_last_elem
