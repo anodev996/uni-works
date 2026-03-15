89887a3015b416e2d462d0fb878fc4a4729704ea
Badulin Ilya <227145436+anodev996@users.noreply.github.com>
2026-03-15 22:18:58 +0700

    feat(sem 4 os): add lab 4 dirs

---
+++ sem-4-os/lab-4/project/src/data_generate.c
@@ -0,0 +1,17 @@
+/* Build and run
+   gcc src/main.c -o build/prog && ./build/prog
+
+   Create .iso for QNX
+   genisoimage -o ~/Downloads/qnx_data.iso \
+   -J -R -V "QNX_DATA" ./src  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main()
+{
+
+    
+    return 0;
+}
