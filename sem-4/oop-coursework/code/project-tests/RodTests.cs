9cec9f7c0f162d7213b5e8426f83d8f2da1399ea
Badulin Ilya <227145436+anodev996@users.noreply.github.com>
2026-05-23 22:46:19 +0700

    feat(oop-cw): add test coverage files

--- sem-4/oop-coursework/code/project-tests/RodTests.cs
+++ sem-4/oop-coursework/code/project-tests/RodTests.cs
@@ -16,14 +16,23 @@ namespace project_tests
             Assert.AreEqual(1, rod.Disks.Count);
             Assert.AreEqual(3, rod.Disks.Peek().Number);
         }
-
         [TestMethod]
-        [ExpectedException(typeof(InvalidOperationException))]
         public void Push_LargerDiskOnSmaller_ThrowsException()
         {
             Rod rod = new Rod();
             rod.Push(new Disk(2));
-            rod.Push(new Disk(1));
+
+            // Современный способ проверки: мы заворачиваем проблемный код в лямбда-выражение
+            Assert.ThrowsException<InvalidOperationException>(() => rod.Push(new Disk(1)));
+        }
+
+        [TestMethod]
+        public void Pop_EmptyRod_ThrowsException()
+        {
+            Rod rod = new Rod();
+
+            // То же самое: ловим ошибку прямо здесь
+            Assert.ThrowsException<InvalidOperationException>(() => rod.Pop());
         }
 
         [TestMethod]
@@ -36,14 +45,6 @@ namespace project_tests
 
             Assert.AreEqual(5, poppedDisk.Number);
             Assert.AreEqual(0, rod.Disks.Count);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(InvalidOperationException))]
-        public void Pop_EmptyRod_ThrowsException()
-        {
-            Rod rod = new Rod();
-            rod.Pop();
         }
     }
 }
